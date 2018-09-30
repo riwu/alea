bd2b5bc2d94dc92c9110f243e50ff06f387c76fa
riwu <me@wangriwu.com>
2018-09-30 12:19:03 +0800

    add basic express server

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.end(); // for health check
+});
+app.get('/favicon.ico', (req, res) => {
+  res.sendStatus(204); // for browser request
+});
+
+app.use(
+  logger((tokens, req, res) =>
+    [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens['response-time'](req, res),
+      'ms',
+      req.headers['x-forwarded-for'],
+      JSON.stringify(req.body)
+    ].join(' ')
+  )
+);
+
+app.use(express.json());
+
+module.exports = app;
