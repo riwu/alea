592434923cd888cdbf74460527173f5e417bfd41
riwu <me@wangriwu.com>
2018-10-21 12:27:20 +0800

    remove hardcoded values in graph

--- mobile/src/components/Graph.js
+++ mobile/src/components/Graph.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { StyleSheet } from 'react-native';
-import {
-  VictoryChart, VictoryLine, VictoryTheme, VictoryAxis,
-} from 'victory-native';
+import { VictoryChart, VictoryLine, VictoryTheme } from 'victory-native';
 import Swiper from 'react-native-swiper';
 import subMonths from 'date-fns/sub_months';
 import format from 'date-fns/format';
@@ -27,37 +25,26 @@ const styles = StyleSheet.create({
 
 const graphData = [
   {
+    id: 0,
+    title: 'Overall Adaptability',
+  },
+  {
     id: 1,
     title: 'Personal Adaptability',
-    values: [2, 1, 4],
   },
   {
     id: 2,
     title: 'Interpersonal Adaptability',
-    values: [1, 3, 1],
   },
   {
     id: 3,
     title: 'Collective Adaptability',
-    values: [2, 1, 4],
   },
   {
     id: 4,
     title: 'Contextual Adaptability',
-    values: [3, 1, 2],
   },
 ];
-
-graphData.unshift({
-  id: 0,
-  title: 'Overall Adaptability',
-  values: graphData.reduce((acc, { values }) => {
-    values.forEach((value, index) => {
-      acc[index] = (acc[index] || 0) + value;
-    });
-    return acc;
-  }, []),
-});
 
 const date = new Date();
 
@@ -67,11 +54,14 @@ const Graph = (props) => {
     acc.push(...adaptabilities.map(id => id));
     return acc;
   }, []);
-  const score = self.concat(feedback).reduce((acc, id) => {
+  const computeScore = arr => arr.reduce((acc, id) => {
     acc[dataMap[id]] = (acc[dataMap[id]] || 0) + 1;
     acc[0] = (acc[0] || 0) + 1;
     return acc;
   }, {});
+
+  const score = computeScore(self.concat(feedback));
+  const selfScore = computeScore(self);
 
   return (
     <Swiper loadMinimal height="auto" activeDotColor="#86BC25">
@@ -93,22 +83,17 @@ const Graph = (props) => {
               style={{
                 data: { stroke: '#86BC25' },
               }}
-              data={info.values
-                .map((value, i) => ({
-                  x: subMonths(date, info.values.length - i),
-                  y: value,
-                }))
-                .concat({
-                  x: date,
+              data={[
+                {
+                  x: format(subMonths(date, 1), 'MMM YY'),
+                  y: selfScore[info.id],
+                },
+                {
+                  x: format(date, 'MMM YY'),
                   y: score[info.id],
-                })
-                .map(value => ({
-                  x: format(value.x, 'MMM YY'),
-                  y: value.y,
-                }))}
+                },
+              ]}
             />
-            <VictoryAxis tickFormat={value => value} />
-            <VictoryAxis tickFormat={value => value} dependentAxis />
           </VictoryChart>
         </Card>
       ))}
